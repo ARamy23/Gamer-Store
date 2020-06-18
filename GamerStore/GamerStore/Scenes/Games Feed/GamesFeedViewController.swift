03b43a5fce6b028f236d9660b4e5be0a82434ebd
Ahmed Ramy <dev.ahmedramy@icloud.com>
2020-06-19 00:15:58 +0200

    Rename and reorganize the files

--- GamerStore/GamerStore/Scenes/Games Feed/GamesFeedViewController.swift
+++ GamerStore/GamerStore/Scenes/Games Feed/GamesFeedViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GamesFeedViewController.swift
 //  GamerStore
 //
 //  Created by Ahmed Ramy on 6/19/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GamesFeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
