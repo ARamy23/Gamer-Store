6d73f0e97611aedcfeb03f419fd86a9e876cb781
Ahmed Ramy <dev.ahmedramy@icloud.com>
2020-06-19 00:00:53 +0200

    Create Xcode project

---
+++ GamerStore/GamerStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GamerStore
+//
+//  Created by Ahmed Ramy on 6/19/20.
+//  Copyright © 2020 Ahmed Ramy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
