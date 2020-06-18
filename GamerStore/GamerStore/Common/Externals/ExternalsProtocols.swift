54665db10fb319326977840fb72a4e0783c6c15a
Ahmed Ramy <dev.ahmedramy@icloud.com>
2020-06-19 01:48:16 +0200

    add router

---
+++ GamerStore/GamerStore/Common/Externals/ExternalsProtocols.swift
@@ -0,0 +1,42 @@
+//
+//  ExternalsProtocols.swift
+//  GamerStore
+//
+//  Created by Ahmed Ramy on 6/19/20.
+//  Copyright © 2020 Ahmed Ramy. All rights reserved.
+//
+
+import UIKit.UIViewController
+
+enum CachingKey: String {
+    case favorites
+}
+
+typealias AlertAction = (title: String, style: UIAlertAction.Style, action: () -> Void)
+
+protocol CacheProtocol {
+    func getData(key: CachingKey) -> [Data]?
+    func saveData(_ data: Data, key: CachingKey)
+    func getObject<T: Codable>(_ object: T.Type, key: CachingKey) -> T?
+    func saveObject<T: Codable>(_ object: T, key: CachingKey)
+    func removeObject(key: CachingKey)
+}
+
+protocol RouterProtocol: class {
+    var presentedView: UIViewController! { set get }
+    func present(view: UIViewController)
+    func startActivityIndicator()
+    func stopActivityIndicator()
+    func dismiss()
+    func pop()
+    func popToRoot()
+    func popTo(vc: UIViewController)
+    func push(view: UIViewController)
+    func alert(title: String, message: String, actions: [(title: String, style: UIAlertAction.Style)])
+    func alertWithAction(title: String?, message: String?, alertStyle: UIAlertController.Style, actions: [AlertAction])
+}
+
+protocol NetworkProtocol: class {
+    func call<T: Codable>(_ endpoint: Endpoint, _ expectedModel: T.Type, onComplete: @escaping ((Result<T, Error>) -> Void))
+}
+
